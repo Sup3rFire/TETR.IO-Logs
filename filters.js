e2d5bd7c274d8b8e7d2ec6bec3aede9f9f843360
SuperFire <me@superfi.re>
2022-08-27 11:48:43 +0700

    Update for new ribbon

--- filters.js
+++ filters.js
@@ -13,12 +13,12 @@ a.click();
 document.body.removeChild(a);
 };` + text;
   text = text.replace(
-    "function SmartEncode(packet) {",
-    "function SmartEncode(packet) {console.log(`[O ${new Date().toLocaleString()}] ${JSON.stringify(packet)}`); packetLogs += `[O ${new Date().toLocaleString()}] ${JSON.stringify(packet)}\\n`;"
+    "function SmartEncode(packet, packr = null, extensionData = null) {",
+    "function SmartEncode(packet, packr = null, extensionData = null) {console.log(`[O ${new Date().toLocaleString()}] ${JSON.stringify(packet)}`); packetLogs += `[O ${new Date().toLocaleString()}] ${JSON.stringify(packet)}\\n`;"
   );
   text = text.replace(
-    "function SmartDecode(packet) {",
-    "function SmartDecode(packet) {const data = SmartDecoder(packet); console.log(`[I ${new Date().toLocaleString()}] ${JSON.stringify(data)}`); packetLogs += `[I ${new Date().toLocaleString()}] ${JSON.stringify(data)}\\n`; return data;};function SmartDecoder(packet) {"
+    "function SmartDecode(packet, unpackr = null) {",
+    "function SmartDecode(packet, unpackr = null) {const data = SmartDecoder(packet); console.log(`[I ${new Date().toLocaleString()}] ${JSON.stringify(data)}`); packetLogs += `[I ${new Date().toLocaleString()}] ${JSON.stringify(data)}\\n`; return data;};function SmartDecoder(packet) {"
   );
   return text;
 });
